41980b2f831b12e4070cb9871968a3ee32a24a11
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2024-12-30 19:41:13 -0600

    burnt orange \m/

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,16 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}',
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'burnt-orange': {
+          DEFAULT: '#CC5500',
+        },
+      },
+    },
   },
   plugins: [],
 };
